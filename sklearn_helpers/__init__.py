f4e22afc6d2623ed92faef56d1baa075b59782d9
David Edward Weirich <weirich.david@gmail.com>
2018-04-22 19:07:14 -0400

    Added RandomizedSearchOOB to __init__.py.

--- sklearn_helpers/__init__.py
+++ sklearn_helpers/__init__.py
@@ -1,8 +1,10 @@
 from .quantile_calibrator import QuantileCalibrator
 from .random_forest_transformer import RandomForestTransformer
+from .randomized_search_oob import RandomizedSearchOOB
+
 
 __all__ = [
     'QuantileCalibrator',
     'RandomForestTransformer',
-    'R'
+    'RandomizedSearchOOB'
 ]
