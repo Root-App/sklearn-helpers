df2c80a1572fdf0ce9723ee384616dbc521966af
David Edward Weirich <weirich.david@gmail.com>
2018-04-22 18:43:18 -0400

    Added RandomForestTransformer class.

---
+++ sklearn_helpers/random_forest_transformer.py
@@ -0,0 +1,22 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.base import TransformerMixin
+
+
+class RandomForestTransformer(RandomForestRegressor, TransformerMixin):
+    """
+    Wrapper to allow the use of RandomForestRegressors as a transformer.
+
+    If sklearn ever adds the ability to apply post-processing to predictors (regressors/classifiers)
+    in a pipeline then this hacky nonsense will not be needed.
+    """
+
+    def transform(self, X, y=None):
+        """
+        Apply the fit random forest to transform an input array into an array of predictions.
+        This is a wrapper for RandomForestRegressor.predict()
+
+        :param X: Input array
+        :param y: Not used, but required for compatability with sklearn's TransformerMixin API.
+        :return: The predictions.
+        """
+        return self.predict(X)
